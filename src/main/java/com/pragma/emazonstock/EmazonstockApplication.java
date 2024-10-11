94460986eef403d3ca7581d017dff906b92fda01
IngrithR14 <nenita.yr34@gmail.com>
2024-10-11 11:02:37 -0500

    primer commit

---
+++ src/main/java/com/pragma/emazonstock/EmazonstockApplication.java
@@ -0,0 +1,13 @@
+package com.pragma.emazonstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmazonstockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmazonstockApplication.class, args);
+	}
+
+}
